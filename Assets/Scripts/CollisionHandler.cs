51ec87a5c981171b5f4c900c24cd891606318fd2
jduke44 <jimduke@gmail.com>
2021-03-07 11:33:02 -0500

    FalconOne game initial commit

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    [SerializeField] float reloadDelay = 1f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip landingPad;
+
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem landingPadParticles;
+
+  
+    AudioSource m_audioSource;
+    
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+    void Start() 
+    {
+        m_audioSource = GetComponent<AudioSource>();
+     
+    }
+
+    void Update() 
+    {
+        DebugKeys();
+    }
+
+    void DebugKeys()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; //toggles variable on and off.
+            
+        }
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+        if (isTransitioning || collisionDisabled) { return;} //if set true, just return and ignore the everything below.
+
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("You have hit a Friendly object");
+                break;
+            case "Finish":
+                FinishSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartCrashSequence()
+    {
+        
+        isTransitioning = true;
+        m_audioSource.Stop();
+        m_audioSource.PlayOneShot(crashSound);
+        crashParticles.Play();
+        //todo add particle sequence.
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", reloadDelay);
+    }
+
+    void FinishSequence()
+    {
+        isTransitioning = true;
+        m_audioSource.Stop();
+        m_audioSource.PlayOneShot(landingPad);
+        landingPadParticles.Play();
+        //todo add particle sequence.
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", reloadDelay);      
+    }
+
+    void ReloadLevel()
+    {
+        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+
+    void LoadNextLevel()
+    {
+        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // PARAMETERS - Things that can be tuned in the editor.
+
+    // CACHE - e.g. References for readability or speed of loading.
+
+    // STATE - private instances (member) variables.
+
+    [SerializeField] float mainThruster = 100f;
+    [SerializeField] float rotateThrust = 1f;
+    [SerializeField] AudioClip mainEngine;
+
+    
+    [SerializeField] ParticleSystem mainThrustParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+    [SerializeField] ParticleSystem rightThrustParticles;
+    Rigidbody rb;
+    AudioSource m_audioSource;
+
+    //bool isThrustOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+       m_audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+        void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartThrusting();
+        }
+        else
+            StopThrusting();
+
+    }
+
+
+
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThruster * Time.deltaTime); //Vector3.up is shorthand for (0,1,0) coordinates.
+        if (!m_audioSource.isPlaying)
+        {
+            m_audioSource.PlayOneShot(mainEngine);
+            //mainThrustParticles.Play();
+        }
+        if (!mainThrustParticles.isPlaying)
+        {
+            mainThrustParticles.Play();
+        }
+    }  
+    private void StopThrusting()
+    {
+        m_audioSource.Stop();
+        mainThrustParticles.Stop();
+    }
+    
+
+
+
+    void ProcessRotation()
+    {
+        ProcessRotating();
+    }
+
+    private void ProcessRotating()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Debug.Log("Rotate Left");
+            ApplyRotation(rotateThrust);
+            if (!rightThrustParticles.isPlaying)
+            {
+                rightThrustParticles.Play();
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //Debug.Log(" Rotate Right");
+            ApplyRotation(-rotateThrust);
+            if (!leftThrustParticles.isPlaying)
+            {
+                leftThrustParticles.Play();
+            }
+            else
+            {
+                rightThrustParticles.Stop();
+                leftThrustParticles.Stop();
+            }
+
+        }
+    }
+
+    void ApplyRotation(float rotateThisFrame)
+    {
+        rb.freezeRotation = true; // freeze rotation so we can manually collide with objects and still maintain control.
+        transform.Rotate(Vector3.forward * rotateThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreeze rotation so the global physics system can take over again.
+    }
+}
