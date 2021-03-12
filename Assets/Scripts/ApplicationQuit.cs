a7c788cfa403087bf17ad4ca4825d19e49f250f4
jduke44 <jimduke@gmail.com>
2021-03-12 08:10:41 -0500

    added quit game fuction, and some final touches to prefabs and first level.

---
+++ Assets/Scripts/ApplicationQuit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplicationProcessQuit();
+    }
+
+    void ApplicationProcessQuit() 
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("escape key hit, game quit");
+        }    
+    }
+
+}
